5ac9ce364523e9fea4524feee33c5464943d6f7f
J_odion <emmyjaff22@gmail.com>
2024-12-26 12:52:32 +0100

    same

--- src/models/table.js
+++ src/models/table.js
@@ -2,12 +2,30 @@ const mongoose = require("mongoose");
 
 // Define Schema
 const tableDataSchema = new mongoose.Schema({
-  site: { type: String, required: true },
-  houseType: { type: String, required: true },
-  purpose: { type: String, required: true },
-  material: { type: String, required: true },
-  subMaterial: { type: String, default: null },
-  date: { type: Date, required: true },
+  site: { 
+    type: String, 
+    required: true 
+  },
+  houseType: { 
+    type: String, 
+    required: true 
+  },
+  purpose: { 
+    type: String, 
+    required: true 
+  },
+  material: { 
+    type: String, 
+    required: true 
+  },
+  subMaterial: { 
+    type: String, 
+    default: null 
+  },
+  date: { 
+    type: Date, 
+    required: true 
+  },
 });
 
 // Create Model
